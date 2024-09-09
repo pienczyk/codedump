02106d5bec68910f08933056d33c911d37d1bc65
pienczyk <88761512+pienczyk@users.noreply.github.com>
2024-09-09 11:44:04 +0200

    Update main.tf
    example -> main

--- test_path/main.tf
+++ test_path/main.tf
@@ -1,24 +1,24 @@
 # Simple TF code
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "main" {
+  name     = "main-resources"
   location = "West Europe"
 }
 
-resource "azurerm_cdn_profile" "example" {
-  name                = "example-cdn"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_cdn_profile" "main" {
+  name                = "main-cdn"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
   sku                 = "Standard_Verizon"
 }
 
-resource "azurerm_cdn_endpoint" "example" {
-  name                = "example"
-  profile_name        = azurerm_cdn_profile.example.name
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_cdn_endpoint" "main" {
+  name                = "main"
+  profile_name        = azurerm_cdn_profile.main.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   origin {
-    name      = "example"
+    name      = "main"
     host_name = "www.contoso.com"
   }
 }
